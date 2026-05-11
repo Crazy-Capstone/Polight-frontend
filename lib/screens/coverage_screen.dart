366b40f262e026ac05404efaa6cef2770b0f730d
ryujeeKim <ryujee0512@naver.com>
2026-05-11 15:42:17 +0900

    [feat]기본 세팅 및 Nav 바 구현완료

---
+++ lib/screens/coverage_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CoverageScreen extends StatelessWidget {
+  const CoverageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('보장내역')),
+    );
+  }
+}
